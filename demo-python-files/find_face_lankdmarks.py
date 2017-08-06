12dac9653fbeeed7e61d9d806dbebccfd14bc7f5
anubhavshrimal <anubhavshrimal@gmail.com>
2017-08-06 22:00:00 +0530

    Corrected model path

--- demo-python-files/find_face_lankdmarks.py
+++ demo-python-files/find_face_lankdmarks.py
@@ -4,7 +4,7 @@ from skimage import io
 
 # You can download the required pre-trained face detection model here:
 # http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-predictor_model = "shape_predictor_68_face_landmarks.dat"
+predictor_model = "../models/shape_predictor_68_face_landmarks.dat"
 
 # Take the image file name from the command line
 file_name = sys.argv[1]
